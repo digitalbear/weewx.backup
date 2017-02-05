513c30d7d9396b98b23efb2becb1248d61e6b3a3
Andrew Cruickshank <acruickshank42@gmail.com>
2017-02-05 23:10:34 +0000

    initial commit

---
+++ s3_archive.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+echo '-------------------------------------------------'
+echo `date` - start S3 archive
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+
+BACKUP_FILE=$1
+S3_BUCKET=$2
+retn_code=0
+
+echo 'copy ' $BACKUP_FILE ' to S3 daily bucket ' $S3_BUCKET
+s3cmd put $BACKUP_FILE $S3_BUCKET/daily/
+
+if [ $? -eq 0 ]; then
+  echo 'remove daily backups from S3 more than 1 week old'
+  s3cmd ls $S3_BUCKET/daily/ | awk '$4 != "$S3_BUCKET/daily/"' | awk '$1 < "'$(date +%F -d '1 week ago')'" {print $4;}' | xargs --no-run-if-empty s3cmd del
+else
+  retn_code=1
+fi
+
+###################################
+# take weekly backup every Sunday #
+###################################
+if [ `date +%w` == "0" ]; then
+
+  echo 'copy ' $BACKUP_FILE ' to S3 weekly bucket'
+  s3cmd put $BACKUP_FILE $S3_BUCKET/weekly/
+
+  if [ $? -eq 0 ]; then
+    echo 'remove weekly backups from S3 more than 1 month old'
+    s3cmd ls $S3_BUCKET/weekly/ | awk '$4 != "$S3_BUCKET/weekly/"' | awk '$1 < "'$(date +%F -d '1 month ago')'" {print $4;}' | xargs --no-run-if-empty s3cmd del
+  else
+    retn_code=1
+  fi
+
+fi
+
+#################################################
+# take monthly backup on the 1st of every month #
+#################################################
+if [ `date +%d` == "01" ]; then
+
+  echo 'copy ' $BACKUP_FILE ' to S3 monthly bucket'
+  s3cmd put $BACKUP_FILE $S3_BUCKET/monthly/
+
+  if [ $? -eq 0 ]; then
+    echo 'remove monthly backups from S3 more than 1 year old'
+#    s3cmd ls $S3_BUCKET/monthly/ | awk '$4 != "$S3_BUCKET/monthly/"' | awk '$1 < "'$(date +%F -d '1 year ago')'" {print $4;}' | xargs --no-run-if-empty s3cmd del
+  else
+    retn_code=1
+  fi
+
+fi
+
+############################################
+# take annual backup on the 1st of January #
+############################################
+if [ `date +%m%d` == "0101" ]; then
+
+  echo 'copy ' $BACKUP_FILE ' to S3 annual bucket'
+  s3cmd put $BACKUP_FILE $S3_BUCKET/annual/
+
+  if [ $? -ne 0 ]; then
+    retn_code=1
+  fi
+fi
+
+echo `date` - end S3 archive
+echo '-------------------------------------------------'
+
+exit $retn_code
---
+++ weewx_backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo '-------------------------------------------------'
+echo `date` - start weewx db backup
+
+export PATH=.:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+
+WEEWX_DB=/home/weewx/ws2083/archive/weewx.sdb
+TODAY=`date +"%Y-%m-%d"`
+DUMP_FILE=/home/weewx/backup/weewx.dump.$TODAY.gz
+S3_BUCKET=s3://dubweather-backup
+
+echo 'stop the weewx daemon and wait 30 seconds'
+service weewx stop
+retn_code=$?
+if [ $retn_code -ne 0 ]; then
+  exit 7
+fi
+
+sleep 30s
+
+echo 'dump sqlite3 database'
+echo '.dump' | sqlite3 $WEEWX_DB | gzip -c > $DUMP_FILE
+
+echo 'restart the weewx daemon'
+service weewx start
+
+# call script to manage daily/weekly/monthly/annual archiving to S3
+s3_archive.sh $DUMP_FILE $S3_BUCKET
+
+retn_code=$?
+if [ $retn_code -eq 0 ]; then
+  echo 'dump successfully copied to S3 - deleting from local'
+  rm -f $DUMP_FILE
+fi
+
+echo `date` - end weewx db backup
+echo '-------------------------------------------------'
